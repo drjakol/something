527b8e259fc5f4d6fe60c8a218b183c901b2d492
drjakol <bhonar.com@gmail.com>
2026-01-05 12:28:42 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import time
+import os
+from telegram import Bot
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
 print("Bot is starting...")
+
+bot = Bot(token=BOT_TOKEN)
+
+bot.send_message(
+    chat_id=CHANNEL_ID,
+    text="✅ Bot successfully started and is running on server."
+)
+
+while True:
+    time.sleep(60)
