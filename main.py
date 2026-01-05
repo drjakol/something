5eedb66b06cec6b024693eb402373e8b175e5798
drjakol <bhonar.com@gmail.com>
2026-01-05 12:34:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-import time
+import asyncio
 import os
 from telegram import Bot
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
-print("Bot is starting...")
+async def main():
+    print("Bot is starting...")
 
-bot = Bot(token=BOT_TOKEN)
+    bot = Bot(token=BOT_TOKEN)
 
-bot.send_message(
-    chat_id=CHANNEL_ID,
-    text="✅ Bot successfully started and is running on server."
-)
+    await bot.send_message(
+        chat_id=CHANNEL_ID,
+        text="✅ Bot successfully started and is running on server."
+    )
 
-while True:
-    time.sleep(60)
+    while True:
+        await asyncio.sleep(60)
+
+if __name__ == "__main__":
+    asyncio.run(main())
