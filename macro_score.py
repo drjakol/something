e91e0c6954d3808d90c62bf8f29bd41bd38f3ecc
drjakol <bhonar.com@gmail.com>
2026-01-06 10:25:49 +0330

    Update macro_score.py

--- macro_score.py
+++ macro_score.py
@@ -1,22 +1,41 @@
-def macro_score(oi, long_short, liquidations, etf):
+from options_max_pain import max_pain_bias
+from oi_divergence import oi_divergence
+from etf_lag import etf_lag_bias
+
+def macro_score(
+    price_change,
+    oi_data,
+    ls_ratio,
+    liq,
+    options,
+    etf,
+    price
+):
     score = 0
 
-    if oi and oi.get("change", 0) > 0:
-        score += 10
+    # OI change
+    oi_change = oi_data.get("change") if oi_data else None
+    score += 10 if oi_change and oi_change > 0 else 0
 
-    if long_short:
-        if long_short.get("longRatio", 0) > 0.55:
+    # OI Divergence
+    score += oi_divergence(price_change, oi_change)
+
+    # Long / Short crowd
+    if ls_ratio:
+        if ls_ratio.get("longRatio", 0) > 0.6:
             score -= 5
-        elif long_short.get("shortRatio", 0) > 0.55:
+        elif ls_ratio.get("shortRatio", 0) > 0.6:
             score += 5
 
-    if liquidations and liquidations.get("total", 0) > 50_000_000:
+    # Liquidation spike
+    if liq and liq.get("total", 0) > 50_000_000:
         score += 10
 
+    # Options Max Pain
+    score += max_pain_bias(options, price)
+
+    # ETF lag
     if etf:
-        if etf.get("netFlow", 0) > 0:
-            score += 20
-        elif etf.get("netFlow", 0) < 0:
-            score -= 20
+        score += etf_lag_bias(etf.get("netFlow"))
 
     return score
