c80d8ece56f6dd3cd666170c85ec15e447504f2e
drjakol <bhonar.com@gmail.com>
2026-01-06 19:42:41 +0330

    Update score_engine.py

--- score_engine.py
+++ score_engine.py
@@ -1,7 +1,9 @@
 from adaptive_weights import adaptive_weight
 
 def smart_score_v2(base_score, winrate):
+    """
+    محاسبه Smart Score با وزن Adaptive و محدودیت 0-100
+    """
     weight = adaptive_weight(winrate)
     raw_score = base_score * weight
-    # محدود کردن Score به 0-100
     return round(max(0, min(100, raw_score)), 2)
