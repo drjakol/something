2cb5b7e3574dc66465b5461e90b3f43fc0884422
drjakol <bhonar.com@gmail.com>
2026-01-06 09:59:14 +0330

    Update session_filter.py

--- session_filter.py
+++ session_filter.py
@@ -1,9 +1,18 @@
 from datetime import datetime, timezone
 
 def active_session():
+    """
+    Trading Sessions (UTC)
+    Asia:     00:00 – 06:59
+    London:   07:00 – 11:59
+    New York: 13:00 – 17:59
+    """
+
     hour = datetime.now(timezone.utc).hour
 
-    if 7 <= hour <= 11:
+    if 0 <= hour <= 6:
+        return "Asia"
+    elif 7 <= hour <= 11:
         return "London"
     elif 13 <= hour <= 17:
         return "New York"
